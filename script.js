22bcc873fdf467201365b66fa9b305cb2d853c12
nikhilyadav566 <nikhilyadav566>
2025-07-04 12:47:57 +0530

    update files

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ const closeModal = () => {
   modal.classList.remove("active");
   overlay.classList.remove("overlayActive");
 };
+
+const copyProfileLink = () => {
+  navigator.clipboard.writeText("https://profile-codewiznik.onrender.com");
+  alert("Profile link copied to clipboard!");
+};
+
+
+//   Typing  Element
+
+const typingElement = document.getElementById("typing");
+const phrases = ["Frontend Developer", "MERN Stack Learner", "Tech YouTuber"];
+let i = 0, j = 0, currentPhrase = [], isDeleting = false;
+
+function loop() {
+  typingElement.textContent = currentPhrase.join("");
+
+  if (!isDeleting && j <= phrases[i].length) {
+    currentPhrase.push(phrases[i][j]);
+    j++;
+  }
+
+  if (isDeleting && j > 0) {
+    currentPhrase.pop();
+    j--;
+  }
+
+  if (j === phrases[i].length) {
+    isDeleting = true;
+    setTimeout(loop, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % phrases.length;
+  }
+
+  setTimeout(loop, isDeleting ? 50 : 150);
+}
+loop();
